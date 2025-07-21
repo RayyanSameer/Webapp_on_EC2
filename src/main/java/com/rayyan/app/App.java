af795310366c27a35e0568230cae40164221693e
EC2 Default User <ec2-user@ip-172-31-10-88.ap-south-1.compute.internal>
2025-07-21 15:27:41 +0000

    Message changed

---
+++ src/main/java/com/rayyan/app/App.java
@@ -0,0 +1,10 @@
+package com.rayyan.app;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("The String has been changed ! :) git ");
+    }
+}
